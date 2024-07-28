a20e228d9d41b6591c5dc33907745bbcb14b94d3
ana-marz <amluquin@hotmail.es>
2024-07-28 18:39:01 +0200

    navegar a info

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { CountriesService } from './../../services/countries.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  public country?: Country;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))  // hace que este subscrito al resultado del observable
+      )
+      .subscribe((country) => {
+        if (!country) {
+          return this.router.navigateByUrl('');
+        }
+        this.country = country;
+        return;
+      });
+  }
 }
